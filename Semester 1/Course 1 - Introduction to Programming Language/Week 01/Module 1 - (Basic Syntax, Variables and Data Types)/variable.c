dab4bbe3b81403044d4b96916fd1da49d78d0bfd
Tumpa Moni Mim <tumpamoni455@gmail.com>
2025-07-21 23:52:40 +0600

    Variable & Data Type
    Variable & Data Type, declaration, assign, initialize, format specifier, garbage value

---
+++ Semester 1/Course 1 - Introduction to Programming Language/Week 01/Module 1 - (Basic Syntax, Variables and Data Types)/variable.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int main()
+{
+    // int rahim = 100, karim = 200;
+    // printf ("%d %d", karim, rahim);
+
+
+    // float chol = 2.589785;
+    // printf("%f", chol);
+    // printf("%0.3f", chol);
+
+
+    char ami = 'C';
+    printf ("%c", ami);
+    return 0;
+}
