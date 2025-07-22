3c755a2d0968533085498ebed6a6bbaa3ef2b1e5
Tumpa Moni Mim <tumpamoni455@gmail.com>
2025-07-23 00:17:32 +0600

    Limitation of data types

---
+++ Semester 1/Course 1 - Introduction to Programming Language/Week 01/Module 1 - (Basic Syntax, Variables and Data Types)/limitation.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+
+{
+    // int a=1000000000;
+    // long long int b=10000000000000;
+    // printf("%d %lld", a,b);
+    // return 0;
+
+
+
+    float a=2005.25265;
+    double b=22.123456789123456;
+    printf("%f %0.15lf", a,b);
+    return 0;
+
+}
--- Semester 1/Course 1 - Introduction to Programming Language/Week 01/Module 1 - (Basic Syntax, Variables and Data Types)/tempCodeRunnerFile.c
+++
@@ -1,2 +0,0 @@
-int rahim = 100, karim = 200;
-    // printf ("%d %d", karim, rahim);
