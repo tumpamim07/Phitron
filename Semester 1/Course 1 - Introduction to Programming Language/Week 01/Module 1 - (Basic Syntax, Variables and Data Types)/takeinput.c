9a860bd41409aa92e65f3f56cb3db321fb56cfea
Tumpa Moni Mim <tumpamoni455@gmail.com>
2025-07-22 23:39:10 +0600

    Take Input & %

---
+++ Semester 1/Course 1 - Introduction to Programming Language/Week 01/Module 1 - (Basic Syntax, Variables and Data Types)/takeinput.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    // int rahim,karim;
+    // float f;
+    // char c;
+    // scanf("%d %f %c", &rahim, &f, &c);
+    // printf("%d %0.2f %c", rahim, f, c);
+    // return 0;
+
+
+    //To take percentage we can follow 2 steps:
+    //first one
+
+    // int a,b;
+    // char p;
+
+    // scanf("%d%c %d%c",&a,&p,&b,&p);
+    // printf("%d%c %d%c",a,p,b,p);
+    // return 0;
+
+    //second one
+    int a,b;
+    scanf("%d%% %d%%",&a,&b);
+    printf("%d%% %d%%",a,b);
+    return 0;
+}
