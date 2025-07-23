ee239792d114f5f3d89b0ac2c82bbbb614d9f8b7
Tumpa Moni Mim <tumpamoni455@gmail.com>
2025-07-23 23:50:06 +0600

    Relational Operators & Logical Operators

---
+++ Semester 1/Course 1 - Introduction to Programming Language/Week 01/Module 2 - (Operators, Conditional Statements)/relational.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a = 3, b = 20;
+
+    printf("a < b : %d\n", a < b);
+    printf("a <= b : %d\n", a <= b);
+    printf("a > b : %d\n", a > b);
+    printf("a >= b : %d\n", a >= b);
+    printf("a == b : %d\n", a == b);
+    printf("a != b : %d\n", a != b);
+
+    return 0;
+}
