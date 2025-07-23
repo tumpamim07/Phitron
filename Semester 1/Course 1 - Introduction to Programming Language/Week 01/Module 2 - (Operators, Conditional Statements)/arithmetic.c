0f5579cf2b80405e0248004fee3fc7283befb83f
Tumpa Moni Mim <tumpamoni455@gmail.com>
2025-07-23 23:20:16 +0600

    Arithmetic Operators

---
+++ Semester 1/Course 1 - Introduction to Programming Language/Week 01/Module 2 - (Operators, Conditional Statements)/arithmetic.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a=3, b=20;
+    // int sum=a+b;
+    // int mns=a-b;
+    // int mlt=a*b;
+    // float dvd= b*1.0/a;
+    int mod=b%a;
+
+    // printf("%d", sum);
+    // printf("%d", mns);
+    // printf("%d",mlt);
+    // printf("%f", dvd);
+    printf("%d",mod); 
+    return 0;
+}
