f7f5a6b28d0f21f73ec7b679b57f64f5233a5724
Tumpa Moni Mim <tumpamoni455@gmail.com>
2025-07-24 23:52:16 +0600

    If Else Condition

---
+++ Semester 1/Course 1 - Introduction to Programming Language/Week 01/Module 2 - (Operators, Conditional Statements)/conditional_1.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int tk;
+    scanf("%d",&tk);
+    // if(tk == 100)
+    // {
+    //     printf("Burger Khabo");
+    // }
+    // else
+    // {
+    //     printf("Khabo Na");
+    //     return 0;
+    // }
+
+
+
+    if (tk >= 100)
+    {
+        printf("Burger Khabo");
+    }
+    else
+    {
+        printf("Khabo Na");
+    }
+}
