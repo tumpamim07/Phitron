76056404282c5eefd1b87c30231e4326e5f90c10
Tumpa Moni Mim <tumpamoni455@gmail.com>
2025-07-25 16:08:26 +0600

    If Condition

---
+++ Semester 1/Course 1 - Introduction to Programming Language/Week 01/Module 2 - (Operators, Conditional Statements)/conditional_4.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int tk;
+    scanf("%d", &tk);
+    // if(tk >= 1000);
+    // {
+    //     printf("Pizza Khabo\n");
+    // }
+
+    // if(tk >= 2000)
+    // {
+    //     printf("Ghurte Jabo\n");
+    // }
+
+
+    //for one line, If it is more than one line, then you need to put it in brackets
+    if(tk >= 1000) 
+    printf("Pizza Khabo\n");
+    
+    return 0;
+}
