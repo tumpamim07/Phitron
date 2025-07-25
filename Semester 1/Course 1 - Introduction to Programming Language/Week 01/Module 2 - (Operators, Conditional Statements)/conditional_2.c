64b500874b8c2369cf6efd26b6b0e4e99e62c808
Tumpa Moni Mim <tumpamoni455@gmail.com>
2025-07-25 13:31:52 +0600

    If Else Ladder

---
+++ Semester 1/Course 1 - Introduction to Programming Language/Week 01/Module 2 - (Operators, Conditional Statements)/conditional_2.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int tk;
+    scanf("%d", &tk);
+    
+    if (tk >= 100)
+    {
+        printf("Burger Khabo");
+    }
+
+    else if(tk >= 50)
+    {
+        printf("Fuchka Khabo");
+    }
+    
+    else if(tk >= 20)
+    {
+        printf("Ice Cream Khabo");
+    }
+
+    else
+    {
+        printf("Khabo Na, Basay jabo");
+    }
+    return 0;
+}
