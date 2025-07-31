16e5566b9b6d3ed84bfc810c2980b4712c455669
Tumpa Moni Mim <tumpamoni455@gmail.com>
2025-07-31 23:15:55 +0600

    Sum from 1 to n

--- Semester 1/Course 1 - Introduction to Programming Language/Week 01/Module 3 - (Loop)/loop_sum.c
+++ Semester 1/Course 1 - Introduction to Programming Language/Week 01/Module 3 - (Loop)/loop_sum.c
@@ -1,12 +1,42 @@
 #include <stdio.h>
 int main()
 {
-    int sum;
+    // int sum=0;
+    // int i;
+    // for(i=1; i<=5; i=i+1)
+    // {
+    //     sum+=i;
+    // }
+    // printf("%d", sum);
+    // return 0;
+
+
+    // int sum=0;
+    // int n;
+    // int i;
+    // scanf("%d", &n);
+
+    // for(i=1; i<=n; i+=1)
+    // {
+    //     sum+=i;
+    // }
+    // printf("%d",sum);
+    // return 0;
+
+
+
+    long long int sum=0;
+    int n;
     int i;
-    for(i=1; i<=5; i=i+1)
+    scanf("%d",&n);
+
+    for(i=1; i<=n; i+=1)
     {
         sum+=i;
     }
-    printf("%d", sum);
+    printf("%lld",sum );
+
     return 0;
+
+
 }
